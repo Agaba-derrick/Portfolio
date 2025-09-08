822789229fa79dab4221f5f7c0061e5482fff5ec
Agaba Derrick <agabaderrick18@gmail.com>
2025-09-08 16:25:46 +0300

    change index to right folder
    Signed-off-by: Agaba Derrick <agabaderrick18@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <div>
+    <div style={{ width: "100%", height: "100vh" }}>
       <iframe
         src="/index.html"
         style={{
           width: "100%",
-          height: "100vh",
+          height: "100%",
           border: "none",
           display: "block",
         }}

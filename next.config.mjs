ffb2eebaa0645eb6bff64f8b011a753361988891
Agaba-derrick <agabaderrick18@gmail.com>
2025-10-08 17:28:51 +0300

    style: format after merge

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio/",
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
